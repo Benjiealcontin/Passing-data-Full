c6cef5d407b8495be83653c25b42769b3be3a0ec
Benjiealcontin <Benjiealcontin23@gmail.com>
2022-09-11 15:28:37 +0800

    second

---
+++ Server/src/main/java/com/operation/If/statement/IfStatementApplication.java
@@ -0,0 +1,13 @@
+package com.operation.If.statement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfStatementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfStatementApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/operation/If/statement/IfStatementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operation.If.statement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfStatementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
